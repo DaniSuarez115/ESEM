b59a78d412d4dcec92f3e3c4d8bdc824f12646ec
dsuarez@amvix.com <dsuarez@amvix.com>
2024-09-18 22:57:32 -0600

    configuracion ba rota xdxdxd

--- ESEM/urls.py
+++ ESEM/urls.py
@@ -16,15 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views as core_views
 from django.conf import settings
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/',core_views.login, name="login")
 ]
 
-
-
-if settings.DEBUG:
-    from django.conf.urls.static import static
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
