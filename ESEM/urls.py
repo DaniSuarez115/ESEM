06468f5938a1229d3cac5f948201120819e79e4d
dsuarez@amvix.com <dsuarez@amvix.com>
2024-09-19 17:28:49 -0600

    template update

--- ESEM/urls.py
+++ ESEM/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/',core_views.login, name="login")
+    path('login/',core_views.login, name="login"),
+    path('home',core_views.home, name="home")
 ]
 
--- core/views.py
+++ core/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def login(request):
-    return render(request, 'core/html/login.html')
+    return render(request, 'core/html/base.html')
+
+def home(request):
+    return render(request, 'core/html/home.html')
